f48d5df4fe141e9ba8ecce076d38596e34a9fd24
Emre Açar <ea517645@gmail.com>
2023-05-29 06:46:20 +0300

    files organized and documents formatted

--- src/JS/scroll.js
+++ src/JS/scroll.js
@@ -21,15 +21,3 @@ window.addEventListener("scroll", () => {
     navbarBrand.classList.add("text-white");
   }
 });
-
-
-
-// Get the modal
-var modal = document.getElementById("id01");
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-  if (event.target == modal) {
-  modal.style.display = "none";
-  }
-    };
