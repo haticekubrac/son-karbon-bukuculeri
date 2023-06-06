f93471baf5c09d31eabada5fd69d8ed54c61e258
Emre Açar <ea517645@gmail.com>
2023-06-06 19:37:08 +0300

    Some changes made.

--- src/Pages/Shop/src/main.jsx
+++ src/Pages/Shop/src/main.jsx
@@ -3,18 +3,18 @@ import * as ReactDOM from "react-dom/client";
 import Layout from "./Layout"
 import {
   createBrowserRouter,
+  createRoutesFromElements,
+  Route,
   RouterProvider,
 } from "react-router-dom";
 import "./index.css";
 
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout/>,
-    children: []
-  },
-]);
+const router = createBrowserRouter(createRoutesFromElements(
+  <Route element={<Layout/>} path="/shop">
+    
+  </Route>
+));
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
