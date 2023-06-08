b4dcb55378e7483a09c72b8ca2dd3874b8b12abc
Hatice Kübra Çiçek <haticekubracicek7@gmail.com>
2023-06-08 22:31:34 +0300

    certificate created

--- src/JS/main.js
+++ src/JS/main.js
@@ -7,3 +7,23 @@ window.onclick = function (event) {
   modal.style.display = "none";
   }
     };
+
+
+var isim = document.getElementById('isim').value;
+
+function submit(e) {
+  e.preventDefault()  
+  window.location.href = "index.html"
+
+  const isim = document.getElementById('isim').value
+  console.log(isim)
+}
+
+
+
+var randomColor = "";
+var characters = "0123456789abcdef";
+
+for (let i = 0; i < 6; i++) {
+  randomColor += characters[Math.floor(Math.random() * 16)];
+}
