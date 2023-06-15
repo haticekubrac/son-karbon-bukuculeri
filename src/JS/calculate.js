3ec658eea6a81e35c1c7c3356c0a9226ba79cf07
Emre Açar <ea517645@gmail.com>
2023-06-15 08:27:19 +0300

    files divided

--- src/JS/calculate.js
+++ src/JS/calculate.js
@@ -1,36 +1,5 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore, doc, setDoc } from "firebase/firestore";
+import generateCoupon from "./generateCoupon"
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBRKlvpVuVLZDx--1TAP2EuYF2A5AvtOvQ",
-  authDomain: "cw-shop-v2.firebaseapp.com",
-  projectId: "cw-shop-v2",
-  storageBucket: "cw-shop-v2.appspot.com",
-  messagingSenderId: "1001166916253",
-  appId: "1:1001166916253:web:9f8b5064a9921126972274",
-};
-
-// Initialize Firebase app
-const app = initializeApp(firebaseConfig);
-
-const db = getFirestore(app);
-
-async function generateCoupon(percentage) {
-  let randomCoupon = "";
-  let characters = "0123456789abcdefABCDEF";
-
-  // Run for loop to generate coupon randomly
-  for (let i = 0; i < 7; i++) {
-    randomCoupon += characters[Math.floor(Math.random() * 22)];
-  }
-
-  await setDoc(doc(db, "userCoupons", randomCoupon), {
-    code: randomCoupon,
-    percentage,
-  });
-
-  return randomCoupon;
-}
 
 
 function sum(a, b, c, d) {
